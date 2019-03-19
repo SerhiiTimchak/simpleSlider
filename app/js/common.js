a2a9a0d9b904fbb6a61640c2d03c3d54a47ae193
Serhii_Timchak <freemoffice@gmail.com>
2019-03-19 22:24:36 +0200

    Ready simple slider. Prodaction version in dist directory

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,24 @@
-$(function() {
-    // $('body').fadeOut();
-    alert('hello world!');
-});
+window.onload = function() {
+    let images = document.querySelectorAll('#slider .images img');
+    let counter = 0;
+    let btnPrev = document.getElementById('btn-prev');
+    let btnNext = document.getElementById('btn-next');
+    btnPrev.onclick = function() {
+        images[counter].className = '';
+        counter--;
+        if (counter < 0) {
+            counter = images.length - 1;
+        };
+
+        images[counter].className = 'active';
+    };
+
+    btnNext.onclick = function() {
+        images[counter].className = '';
+        counter++;
+        if (counter >= images.length) {
+            counter = 0;
+        }
+        images[counter].className = 'active';
+    }
+}
